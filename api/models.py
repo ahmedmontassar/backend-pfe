6d225816fded9351399bf67b6bbe1c57dc82b15a
ahmedmontassar <125378129+ahmedmontassar@users.noreply.github.com>
2023-04-24 14:25:23 +0000

    V1.0.0

--- api/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-class MyObject:
-    def __init__(self, id, name, description):
-        self.id = id
-        self.name = name
-        self.description = description
-# Create your models here.
--- appmobile/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import MyObjectList
-
-urlpatterns = [
-    path('myobjects/', MyObjectList.as_view()),
-]
